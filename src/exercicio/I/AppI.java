daaf7bbd915f483809eb9e4ceaae0c9dcd916bd2
Tiago Souza <tiago.souza@zup.com.br>
2019-05-20 01:51:16 -0300

    Programa para invertar valores de variáveis

---
+++ src/exercicio/I/AppI.java
@@ -0,0 +1,41 @@
+package exercicio.I;
+
+import java.util.Scanner;
+
+public class AppI {
+ 
+	  public static void main (String[] args) {
+		  
+		 
+		  
+		  int valorA = 9;
+
+		  
+		  int valorB =2;
+		  
+		  
+		  int troca = valorA;
+		  
+		  valorA = valorB;
+		  
+		  valorB = troca;
+		  
+		  System.out.println("valorA é " + valorA);
+		  System.out.println(" valorB é " + valorB);
+		  
+		  
+		  
+		  
+				  
+		  
+		  
+		  
+		
+		  
+		  
+		  
+		  
+		  
+	  }
+	  
+}
